3f3b219c5dc176c0bf51e93416eeae4a2a53cc42
varshasharma01 <varshasinbox1@gmail.com>
2025-09-07 19:38:07 +0530

    upload Salary Prediction model

---
+++ app.py
@@ -0,0 +1,29 @@
+import numpy as np
+from flask import Flask, request, render_template
+import pickle
+
+application = Flask(__name__) #will initialize the flask App
+model = pickle.load(open('model.pkl', 'rb'))
+
+@application.route('/')
+def home():
+    return render_template('index.html')
+
+@application.route('/predict',methods=['POST'])
+
+def predict():
+    
+    # For rendering results on HTML GUI   
+    int_features = [int(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
+
+    output = round(prediction[0], 2)
+
+    return render_template(
+        'index.html',
+        prediction_text=f"Employee Salary should be {output}",
+        input_values=int_features 
+    )
+if __name__ == "__main__":
+    application.run(debug=True)
